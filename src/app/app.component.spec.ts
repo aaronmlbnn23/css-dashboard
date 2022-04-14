5f07a83e93839969425ad5b9b033e84304313a82
Aaron Jay <93783146+aaronmlbnn23@users.noreply.github.com>
2022-04-15 02:37:10 +0800

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'css-dashboard-template'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('css-dashboard-template');
+    //expect(app.title).toEqual('css-dashboard-template');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'css-dashboard-template';
+  constructor() { }
+
+  ngOnInit(): void {
+    let btn = document.querySelector('#toggleBtn');
+    let sidebar = document.querySelector('.sidebar');
+
+    btn?.addEventListener('click', function () {
+      sidebar?.classList.toggle('active');
+    });
+  }
+ 
 }
+
